6dae5984bb3fd35106a5913b994413894f072b9d
dharmaseervi <dharmaseervijb18239@gmail.com>
2024-01-18 22:46:16 +0530

    update commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import WeatherFooter from "./components/WeatherFooter";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+        <WeatherFooter />
       </BrowserRouter>
     </div>
   );
---
+++ src/components/WeatherFooter.jsx
@@ -0,0 +1,9 @@
+const WeatherFooter = () => {
+  return (
+    <footer className="bg-blue-500 text-white p-4 text-center">
+      <p>&copy; dharmaseervi 2024 Weather App</p>
+    </footer>
+  );
+};
+
+export default WeatherFooter;
